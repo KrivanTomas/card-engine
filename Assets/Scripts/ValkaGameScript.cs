122b2e5667ab5669c4b91f64a9f118b1469d18f2
KurtinLukas <kurtinl.04@spst.eu>
2022-10-17 23:14:10 +0200

    Game functionality
    fully working

--- Assets/Scripts/ValkaGameScript.cs
+++ Assets/Scripts/ValkaGameScript.cs
@@ -7,46 +7,112 @@ public class ValkaGameScript : MonoBehaviour
     public CardAreaScript[] handArea;
     public CardAreaScript[] gameArea;
     public CardAreaScript startingDeck;
+    private CardAreaScript allGameCards;
+
+    private Generatepack gen;
     
+    //bool roundEnded = false;
     int playerCount = 2;
-    int playerTurn = 0;
+    //int playerTurn = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        gen = startingDeck.gameObject.GetComponent<Generatepack>();
+        StartGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(handArea[0].cards.Count == 0 || handArea[1].cards.Count == 0){
+            print("The round has ended.");
+            StartGame();
+        }
+    }
+
+    public void StartGame()
+    {
+        handArea[0].cards.Clear();
+        handArea[1].cards.Clear();
+        for(int i = 0; i < gen.cardHideaway.GetComponent<Transform>().childCount-1; i++){
+            Destroy(gen.cardHideaway.GetComponent<Transform>().GetChild(i).gameObject);
+        }
+
         for(int i = 0; i < playerCount; i++){
             handArea[i].ownershipID = i;
-            gameArea[i].ownershipID = i;
         }
-        startingDeck.gameObject.GetComponent<Generatepack>().Generate();
+
+        gen.Generate();
         int count = startingDeck.cards.Count;
         for(int i = 0; i < count; i++){
             if(count / 2 <= i)
                 DrawCardFromDeck(0, true);
             else 
                 DrawCardFromDeck(1, true);
-            
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     public void Action(ClassicCardScript ccs, int playerID)
     {
         //if(playerID != playerTurn) return;
         if(ccs.CardArea == handArea[1]) return;
+        else if(ccs.CardArea != handArea[0]) {
+            CompareCards();
+            return;
+        }
+        else 
+            if(gameArea[0].cards.Count > 0)
+                return;
         
         ccs.hover = false;
         ccs.CardArea.cards[ccs.CardArea.cards.Count-1].hover = true;
-
-        MoveCard(handArea[playerID].cards[handArea[playerID].cards.Count-1], gameArea[playerID]);
+        ccs.selected = false;
+        ccs.CardArea.cards[ccs.CardArea.cards.Count-1].selected = true;
         
-        //super high level AI
-        MoveCard(handArea[1].cards[handArea[1].cards.Count-1], gameArea[1]);
+        Draw();
+    }
+
+    private void CompareCards()
+    {
+        //print(((int)gameArea[0].cards[gameArea[0].cards.Count-1].Card_value+1) + " jmeno: " + gameArea[0].cards[gameArea[0].cards.Count-1].Card_value);
+        int value1 = (int)gameArea[0].cards[gameArea[0].cards.Count-1].Card_value + 1;
+        int value2 = (int)gameArea[1].cards[gameArea[1].cards.Count-1].Card_value + 1;
+
+        if(value1 > value2){
+            CollectCards(0);
+            print("player1 wins");
+        }
+        else if(value1 < value2){
+            CollectCards(1);
+            print("player2 wins");
+        }
+        else {
+            for(int i = 0; i < 3; i++){
+                Draw();
+            }
+        }
+    }
+
+    private void Draw() //draws a card from hand area and places it onto the game area, with the enemy doing the same
+    {
+        MoveCard(handArea[0].cards[handArea[0].cards.Count-1], gameArea[0], false);
+        gameArea[0].totalStacking = gameArea[0].cards.Count * 0.0218f;
+
+        MoveCard(handArea[1].cards[handArea[1].cards.Count-1], gameArea[1], false); //bot
+        gameArea[1].totalStacking = gameArea[1].cards.Count * 0.0218f;
+    }
+
+    private void CollectCards(int playerID) //collects cards from all game areas and inserts them into playerID's hand area
+    {
+        for(int pc = 0; pc < playerCount; pc++)
+        {
+            for(int i = gameArea[pc].cards.Count-1; i >= 0; i--)
+            {
+                MoveCard(gameArea[pc].cards[i], handArea[playerID], true);
+            }
+        }
+        handArea[playerID].Shuffle();
     }
 
     private void MoveCard(ClassicCardScript ccs, CardAreaScript new_cas) {
