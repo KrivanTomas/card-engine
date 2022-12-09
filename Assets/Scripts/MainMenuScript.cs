530b7e93a1af1d9e94ec87e4bc400973cbe33a38
KrivanTomas <krivant.05@spst.eu>
2022-12-09 15:01:29 +0100

    Updated menus

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -2,15 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuScript : MonoBehaviour
 {
-    private int menu = 0;
 
-    public Transform idk;
-    public GameObject menu1;
-    public GameObject menu2;
-    public GameObject menu3;
+    //public Transform idk;
+    public GameObject[] menus;
+    public Slider loadingBar;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,37 +19,46 @@ public class MainMenuScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        switch (menu) {
-            case 0:
-                idk.rotation *= Quaternion.Euler(0f,0f,70f*Time.deltaTime);
-                break;
+        //idk.rotation *= Quaternion.Euler(0f,0f,70f*Time.deltaTime);
+    }
+
+    public void SwitchMenu(int index) {
+        foreach (GameObject item in menus)
+        {
+            item.SetActive(false);
+        }
+        menus[index].SetActive(true);
+    }
+
+    public void LoadGame(int index) {    
+              
+        StartCoroutine(LoadAsync(index));
+    }
+
+    public void LoadGame(string name) {
+        
+        StartCoroutine(LoadAsync(name));
+    }
+
+    IEnumerator LoadAsync (int index) {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(index);
+        SwitchMenu(4);
+        while (!operation.isDone){
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            loadingBar.value = progress;
+            yield return null;
         }
     }
 
-    public void Menu1() {
-        menu1.SetActive(true);
-        menu2.SetActive(false);
-        menu3.SetActive(false);
-    }
-
-    public void Menu2() {
-        menu1.SetActive(false);
-        menu2.SetActive(true);
-        menu3.SetActive(false);
-    }
-
-    public void Menu3() {
-        menu1.SetActive(false);
-        menu2.SetActive(false);
-        menu3.SetActive(true);
-    }
-
-    public void VRoom() {          
-        SceneManager.LoadSceneAsync("ValkaScene");
-    }
-
-    public void ARoom() {
-        SceneManager.LoadSceneAsync("AutobusScene");
+    IEnumerator LoadAsync (string name) {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(name);
+        SwitchMenu(4);
+        while (!operation.isDone){
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            Debug.Log(progress);
+            loadingBar.value = progress;
+            yield return null;
+        }
     }
 
     public void QuitGame() {
